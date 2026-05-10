f9a1a4cae69b4f8ce449223c90694e8cbee3de10
iki1uc <entertrainer@web.de>
2026-05-11 00:07:24 +0200

    Update RAW_WAR_BETA_LOCAL.js

--- RAW_WAR_BETA_LOCAL.js
+++ RAW_WAR_BETA_LOCAL.js
@@ -19,19 +19,35 @@ export function setState(n, bstat, rgrad, hgrad, kgrad) {
     const a = document.getElementById("NC-ANCHOR");
     a.dataset["s" + n] = `${bstat},${rgrad},${hgrad},${kgrad}`;
 }
+import { rawwarKontakt } from "./rawwar-core.js";
+import { renderRawwar } from "./RAWWAR_CORE.js";
 
 export function next() {
     const a = document.getElementById("NC-ANCHOR");
     let n = parseInt(a.dataset.active);
+
     n = n === 4 ? 1 : n + 1;
     a.dataset.active = n;
-    return getState(n);
+
+    const s = getState(n);
+
+    renderRawwar();     // Screen aktualisieren
+    rawwarKontakt();    // Cube reagiert
+
+    return s;
 }
 
 export function prev() {
     const a = document.getElementById("NC-ANCHOR");
     let n = parseInt(a.dataset.active);
+
     n = n === 1 ? 4 : n - 1;
     a.dataset.active = n;
-    return getState(n);
+
+    const s = getState(n);
+
+    renderRawwar();     // Screen aktualisieren
+    rawwarKontakt();    // Cube reagiert
+
+    return s;
 }
