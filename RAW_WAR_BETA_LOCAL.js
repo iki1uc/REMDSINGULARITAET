57f5c6449f4861b1425ae982fab9246fa0b53a4e
iki1uc <entertrainer@web.de>
2026-05-11 00:53:22 +0200

    Update RAW_WAR_BETA_LOCAL.js

--- RAW_WAR_BETA_LOCAL.js
+++ RAW_WAR_BETA_LOCAL.js
@@ -21,7 +21,28 @@ export function setState(n, bstat, rgrad, hgrad, kgrad) {
     a.dataset["s" + n] = `${bstat},${rgrad},${hgrad},${kgrad}`;
 }
 
-import { rawwarKontakt } from "./rawwar-core.js";
+import { next, prev, renderNC, getState } from "./RAW_WAR_BETA_LOCAL.js";
+
+function updateHUDGlow() {
+    const hud = document.getElementById("NC-HUD");
+    const s = getState();
+    hud.className = "r" + s.RGRAD;
+}
+
+document.addEventListener("keydown", e => {
+
+    if (e.key === "ArrowRight") {
+        next();
+    }
+
+    if (e.key === "ArrowLeft") {
+        prev();
+    }
+
+    renderNC();
+    updateHUDGlow();
+});
+
 import { renderRawwar } from "./RAWWAR_CORE.js";
 
 export function next() {
