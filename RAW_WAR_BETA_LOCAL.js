491e23c031c3db75d500e1db84bd33822d9e3df1
iki1uc <entertrainer@web.de>
2026-05-11 00:09:11 +0200

    Update RAW_WAR_BETA_LOCAL.js

--- RAW_WAR_BETA_LOCAL.js
+++ RAW_WAR_BETA_LOCAL.js
@@ -1,25 +1,25 @@
 export function getActive() {
     const a = document.getElementById("NC-ANCHOR");
-    const n = a.dataset.active;
-    return parseInt(n);
+    return parseInt(a.dataset.active);
 }
 
 export function getState(n = getActive()) {
     const a = document.getElementById("NC-ANCHOR");
     const raw = a.dataset["s" + n].split(",");
+
     return {
-        BSTAT: raw[0],
-        RGRAD: raw[1],
-        HGRAD: raw[2],
-        KGRAD: raw[3]
+        BSTAT: parseInt(raw[0]),
+        RGRAD: parseInt(raw[1]),
+        HGRAD: parseInt(raw[2]),
+        KGRAD: parseInt(raw[3]),
+        IDX: n
     };
 }
 
 export function setState(n, bstat, rgrad, hgrad, kgrad) {
     const a = document.getElementById("NC-ANCHOR");
     a.dataset["s" + n] = `${bstat},${rgrad},${hgrad},${kgrad}`;
-}
-import { rawwarKontakt } from "./rawwar-core.js";
+}import { rawwarKontakt } from "./rawwar-core.js";
 import { renderRawwar } from "./RAWWAR_CORE.js";
 
 export function next() {
@@ -31,8 +31,8 @@ export function next() {
 
     const s = getState(n);
 
-    renderRawwar();     // Screen aktualisieren
-    rawwarKontakt();    // Cube reagiert
+    renderRawwar();
+    rawwarKontakt();
 
     return s;
 }
@@ -46,8 +46,9 @@ export function prev() {
 
     const s = getState(n);
 
-    renderRawwar();     // Screen aktualisieren
-    rawwarKontakt();    // Cube reagiert
+    renderRawwar();
+    rawwarKontakt();
 
     return s;
 }
+
