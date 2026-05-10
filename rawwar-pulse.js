c2eac67a47a2c8fe9cb1612aa37f7b102bda6262
iki1uc <entertrainer@web.de>
2026-05-11 01:14:51 +0200

    Update rawwar-pulse.js

--- rawwar-pulse.js
+++ rawwar-pulse.js
@@ -1,1 +1,13 @@
 
+// rawwar-pulse.js
+export function rawwarPulse() {
+    const cube = document.getElementById("cube");
+
+    if (!cube) return;
+
+    cube.classList.add("rawwar-pulse");
+
+    setTimeout(() => {
+        cube.classList.remove("rawwar-pulse");
+    }, 600);
+}
