8a6ee0271b346255ee4211fb41526aaa0aba03de
iki1uc <entertrainer@web.de>
2026-05-17 16:35:44 +0200

    Update verhandlung-core.js

--- verhandlung-core.js
+++ verhandlung-core.js
@@ -1,7 +1,10 @@
+// verhandlung-core.js
 import { next, prev, renderNC, getState } from "./RAW_WAR_BETA_LOCAL.js";
 
 function updateHUDGlow() {
     const hud = document.getElementById("NC-HUD");
+    if (!hud) return;
+
     const s = getState();
     hud.className = "r" + s.RGRAD;
 }
