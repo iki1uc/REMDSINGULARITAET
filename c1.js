099fe94db8b17206a34977acde7fb37b63d03341
iki1uc <entertrainer@web.de>
2026-05-25 19:11:31 +0200

    Update c1.js

--- c1.js
+++ c1.js
@@ -1,27 +1,11 @@
-// c1.js – Cube‑1 Logik
+<body data-cube="1">
 
-// Maske laden
-fetch("mask.html")
-  .then(r => r.text())
-  .then(t => {
-    document.getElementById("mask").innerHTML = t;
-  });
+  <div id="mask"></div>
+  <div id="core"></div>
+  <div id="out"></div>
 
-// Kernbereich initialisieren
-document.getElementById("core").innerHTML = `
-  <div class="core-title">Cube‑1 Kern</div>
-  <div class="core-info">Bereich 1 aktiv</div>
-`;
+  <script src="pipe.js"></script>
+  <script src="c1.js"></script>
+  <script src="out1.js"></script>
 
-// Wissenschaftlicher Output für Cube‑1
-function c1_out(ist, soll) {
-  document.getElementById("out").innerHTML = `
-    <div class="out-title">Wissenschaftlicher Output – Cube‑1</div>
-    <div class="out-ist"><b>IST:</b> ${ist}</div>
-    <div class="out-soll"><b>SOLL:</b> ${soll}</div>
-  `;
-}
-
-// Beispielstart
-c1_out("Initialisiert", "Bereit für Verbund");
-
+</body>
